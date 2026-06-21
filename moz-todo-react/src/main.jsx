8ec126085d087ba6c2fee296c59da22b4bb8ac44
清水北斗 <24aw0124@jec.ac.jp>
2026-06-21 09:37:44 +0900

    navdisplay

--- moz-todo-react/src/main.jsx
+++ moz-todo-react/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Jobs from './compment/jobs/jobs.jsx'
+import Nav from './compment/nav/nav.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Nav />
+    <Jobs />
   </StrictMode>,
 )
