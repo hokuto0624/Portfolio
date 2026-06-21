775f7fda8ed4357d8b3028d8e35660cdff0b8f87
清水北斗 <24aw0124@jec.ac.jp>
2026-06-21 09:37:23 +0900

    Webnav

---
+++ moz-todo-react/src/compment/nav/nav.jsx
@@ -0,0 +1,17 @@
+import './nav.css'
+
+function Nav(){
+
+    return(
+        <>
+        <ul>
+            <li>home</li>
+            <li>library</li>
+            <li>skill</li>
+            <li>mail</li>
+        </ul>
+        </>
+    )
+
+}
+export default Nav
