09587e00f2c49dcad4f26cbac115be63ed5bf769
清水北斗 <24aw0124@jec.ac.jp>
2026-06-28 10:10:44 +0900

    newcompomemt_header

--- moz-todo-react/src/main.jsx
+++ moz-todo-react/src/main.jsx
@@ -1,16 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Header from './compment/header/heder.jsx'
 import Jobs from './compment/jobs/jobs.jsx'
 import Nav from './compment/nav/nav.jsx'
-// import Lib from './compment/library/library.jsx'
-// import Skill from './compment/skill/skill.jsx'
+import Lib from './compment/library/library.jsx'
 // import Mail from './compment/mail/mail.jsx'
 import Footer from './compment/footer/footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Nav />
+    <Header />
+    <Lib />
     <Jobs />
     <Footer />
   </StrictMode>,
